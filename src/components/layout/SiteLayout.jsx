fbf0db2d0ac2601e2865ce59705eb64cb5d29c86
John <johnjerickagapito@gmail.com>
2026-09-09 18:48:59 +0800

    Fix sitelayout to open at the top of the page

--- src/components/layout/SiteLayout.jsx
+++ src/components/layout/SiteLayout.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react'
 import { Outlet, useLocation } from 'react-router-dom'
 import Header from './Header.jsx'
 import Footer from './Footer.jsx'
@@ -5,6 +6,10 @@ import ChatWidget from '../chat/ChatWidget.jsx'
 
 function SiteLayout() {
   const { pathname } = useLocation()
+
+   useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
 
   return (
     <>
